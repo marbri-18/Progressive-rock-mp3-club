7078b5673df9f41580804e8ceab03d1ed61ca443
Martin Bridgman <martin.bridgman@gmail.com>
2021-11-25 14:47:59 +0000

    initial commit

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDENTIALS = Credentials.from_service_account_file("credentials.json")
+CREDENTIALS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
 SHEET = GSPREAD_CLIENT.open("progressive rock mp3 club")
