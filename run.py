c2262aad66d2427fd69b6b9fcde180e1ba57ea24
Martin Bridgman <martin.bridgman@gmail.com>
2021-11-25 00:07:39 +0000

    Set up API connection to google sheet and confirm

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDENTIALS = Credentials.from_service_account_file("credentials.json")
+SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
+SHEET = GSPREAD_CLIENT.open("progressive rock mp3 club")
+
+proto_prog_bands = SHEET.worksheet("Proto-Prog")
+
+data = proto_prog_bands.get_all_values()
+pprint(data)
