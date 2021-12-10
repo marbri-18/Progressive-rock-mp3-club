2c3795a90757337319ca8e4f309b1252f922146f
Martin Bridgman <martin.bridgman@gmail.com>
2021-12-10 09:41:44 +0000

    Add function to compile single band list

--- run.py
+++ run.py
@@ -278,7 +278,19 @@ def get_band_of_week():
     band_of_week = all_bands_list[highest_survey_value_index]
     return band_of_week
 
-
+def compile_all_bands_list():
+    """
+    function calls get band names function to get list of bands 
+    in each of the four category questions.
+    Compiles single list for all bands from four category lists.
+    returns single list to get band of week function.
+    """ 
+    band1 = get_band_names("Proto-Prog")
+    band2 = get_band_names("Classic-Prog")
+    band3 = get_band_names("Neo-Prog")
+    band4 = get_band_names("Contemporary-Prog")
+    all_bands = band1 + band2 + band3 + band4
+    return all_bands
 
 def main():
     """
